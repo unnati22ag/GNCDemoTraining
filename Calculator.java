e8954be85ef14be52edda22db7ade99bc11fcf53
unnati22ag <unnatiagrawal2006@gmail.com>
2026-04-10 09:20:02 +0530

    Add files via upload
    this day 1 commit

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+    public class Calculator{
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter two number:");
+            double a=sc.nextDouble();
+            double b=sc.nextDouble();
+            System.out.println("Sum:"+(a+b));
+            sc.close();
+              }
+}
