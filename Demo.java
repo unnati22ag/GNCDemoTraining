b0b124c20d3fcf32bfb93beff5e85abc56cb5e1c
unnati22ag <unnatiagrawal2006@gmail.com>
2026-04-10 09:31:14 +0530

    Add Speaker class with method overloading

--- Demo.java
+++ Demo.java
@@ -1,5 +1,27 @@
-public class Demo {
-   public static void main(String[] args) {
-       System.out.println("Hello, World!");
-   }
+
+class Speaker {
+    // Method to speak a message
+    public void speak(String message) {
+        System.out.println("Speaker says: " + message);
+    }
+
+    //Overloaded method to speak a message with a volume level
+    public void speak(String message, int volume) {
+        System.out.println("Speaker says: " + message + "at volume " + volume);
+    }
+
+    // Overloaded method to speak a message in a specific language
+    public void speak(String message, String language) {
+        System.out.println("Speaker says: " + message + " in " + language);
+    }
+}
+
+//calling above methods in main method
+public class MethodOverloading{
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker();
+        speaker.speak("Hello,World!");
+        speaker.speak("Hello,World!",5);
+        speaker.speak("Hello,World!","English");
+    }
 }
