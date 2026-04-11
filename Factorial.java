54bf35b20dff569dcd128a787ef8caf81d69337f
unnati22ag <unnatiagrawal2006@gmail.com>
2026-04-11 22:18:20 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,18 @@
 \\find factorial of entered no.
-import java.util.Scanner*;
-public class Factorial(){
+import java.util.Scanner;
+public class Factorial{
     public static void main(String[] args){
       Scanner sc= new Scanner(System.in);
          System.out.println("enter a no.:");
          int number = sc.nextInt();
-         int fact= 1,i;
-           for(i=1;i<=a;i++){
-            fact*=
+         long fact= 1;
+           for(int=1;i<=number;i++){
+            factorial*=i;
            }
-           fact*=i;
-           System.out.println("factorial of "+number);
+           System.out.println("Factorial of " + number + " is: " + factorial);
             sc.close();
-          }}
+          }
+}
 
 
 
